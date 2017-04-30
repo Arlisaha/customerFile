c5fa67315e9d89396ede4acbca28b0bd1dfc8d1d
Yann Di Serio <diserio.yann@gmail.com>
2017-04-30 23:14:49 +0200

    Add fixtures for breed.

---
+++ src/AppBundle/Datafixtures/ORM/LoadBreedData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Breed;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadBreedData implements FixtureInterface
+{
+	private static $breedNameList = [
+		'Berger Allemand',
+		'Shih-tzu',
+		'Bouledogue Français',
+		'Bouledogue Anglais',
+		'Bauceron',
+		'Dalmatien',
+		'Chihuahua',
+		'Yorkshire',
+		'Bouvier Bernois',
+	];
+
+	public function load(ObjectManager $manager)
+	{
+		foreach (self::$breedNameList as $breedName) {
+			$breed = new Breed();
+			$breed->setName($breedName);
+
+			$manager->persist($breed);
+		}
+
+		$manager->flush();
+	}
+}
