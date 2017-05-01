05c80941e379f369d087ba34d5df8b683d9aee60
Yann Di Serio <diserio.yann@gmail.com>
2017-05-01 15:21:53 +0200

    Specie fixtures.

--- src/AppBundle/DataFixtures/ORM/LoadSpecieData.php
+++ src/AppBundle/DataFixtures/ORM/LoadSpecieData.php
@@ -2,8 +2,8 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use AppBundle\Entity\Breed;
-use AppBundle\Entity\Specie;
+use AppBundle\Entity\Animal\Breed;
+use AppBundle\Entity\Animal\Specie;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
