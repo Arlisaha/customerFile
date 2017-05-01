867b4067429c3e3700e12d82e8640028c91848a4
Yann Di Serio <diserio.yann@gmail.com>
2017-05-01 21:41:44 +0200

    Create life style fixtures.

---
+++ src/AppBundle/DataFixtures/ORM/LoadLifeStyleData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Customer\LifeStyle;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadLifeStyleData extends AbstractFixture implements OrderedFixtureInterface
+{
+	private static $lifeStyleTypesList = [
+		'Sportif',
+		'Casanier',
+	];
+
+	public function load(ObjectManager $manager)
+	{
+		foreach (self::$lifeStyleTypesList as $lifeStyleType) {
+			$lifeStyle = new LifeStyle();
+			$lifeStyle->setLabel($lifeStyleType);
+
+			$manager->persist($lifeStyle);
+		}
+
+		$manager->flush();
+	}
+
+	public function getOrder()
+	{
+		return 4;
+	}
+}
