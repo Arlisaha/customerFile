20e4819b5a5c6a050fcd761169ae7ebf472adab6
Yann Di Serio <diserio.yann@gmail.com>
2017-05-05 23:08:29 +0200

    Basic js to add form types.

---
+++ web/public/js/customer_card/form_handler.js
@@ -0,0 +1,25 @@
+/*var ownersData = {
+    id: '#customer_card_customer_owners',
+    prototype: '__owner__',
+    count: '{{ form.customer.owners|length }}'
+};*/
+
+function addTypePart(e, data) {
+    e.preventDefault();
+
+    var typePart = $(data['id']);
+
+    var newWidget = typePart.attr('data-prototype');
+    newWidget = newWidget.replace(new RegExp(data['prototype'], 'g'), data['count']);
+    data['count']++;
+
+    var newLi = $('<li></li>').html(newWidget);
+    newLi.appendTo(typePart);
+}
+
+$(document).ready(function() {
+    $('#add_owner').click(function(e) {addTypePart(e, ownersData)});
+    $('#add_cat').click(function(e) {addTypePart(e, catsData)});
+    $('#add_dog').click(function(e) {addTypePart(e, dogsData)});
+    $('#add_consultation').click(function(e) {addTypePart(e, consultationsData)});
+})
