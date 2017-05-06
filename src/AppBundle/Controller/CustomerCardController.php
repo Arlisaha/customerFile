17e33143302125d913272406914dd7f0ea06bba3
Yann Di Serio <diserio.yann@gmail.com>
2017-05-07 00:57:38 +0200

    Changed logic.

--- src/AppBundle/Controller/CustomerCardController.php
+++ src/AppBundle/Controller/CustomerCardController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\CustomerCard\CustomerCard;
 use AppBundle\Form\CustomerCard\CustomerCardType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,10 +24,10 @@ class CustomerCardController extends Controller
 	 *
 	 * @Route("/", name="customer_card")
 	 */
-    public function indexAction($name)
-    {
-        return $this->render('', array('name' => $name));
-    }
+	public function indexAction($name)
+	{
+		return $this->render('', ['name' => $name]);
+	}
 
 	/**
 	 * @param Request $request
@@ -49,48 +50,39 @@ class CustomerCardController extends Controller
 	}
 
 	/**
-	 * @param Request $request
+	 * @param Request      $request
+	 * @param CustomerCard $customerCard
 	 *
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 *
-	 * @Route("/new", name="customer_card_new")
+	 * @Route("/get/{id}", requirements={"id" = "\d+"}, name="customer_card_get")
+	 * @ParamConverter("customerCard", class="AppBundle:CustomerCard\CustomerCard")
 	 */
-    public function newAction(Request $request)
+	public function getAction(Request $request, CustomerCard $customerCard)
 	{
-		$customerCard = new CustomerCard();
+	}
 
+	/**
+	 * @param Request      $request
+	 * @param CustomerCard $customerCard
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 *
+	 * @Route("/edit/{id}", requirements={"id" = "\d+"}, name="customer_card_edit")
+	 * @ParamConverter("customerCard", class="AppBundle:CustomerCard\CustomerCard")
+	 */
+	public function editAction(Request $request, CustomerCard $customerCard)
+	{
 		$form = $this->createForm(CustomerCardType::class, $customerCard, []);
 
 		$form->handleRequest($request);
 
-		if($form->isSubmitted() && $form->isValid()) {
+		if ($form->isSubmitted() && $form->isValid()) {
 
 		}
 
-		return $this->render(':customer_card:new.html.twig', [
+		return $this->render(':customer_card:edit.html.twig', [
 			'form' => $form->createView(),
 		]);
-	}
-
-	/**
-	 * @param Request $request
-	 * @param         $id
-	 *
-	 * @Route("/get/{id}", requirements={"id" = "\d+"}, name="customer_card_get")
-	 */
-	public function getAction(Request $request, $id)
-	{
-
-	}
-
-	/**
-	 * @param Request $request
-	 * @param         $id
-	 *
-	 * @Route("/edit/{id}", requirements={"id" = "\d+"}, name="customer_card_edit")
-	 */
-	public function editAction(Request $request, $id)
-	{
-
 	}
 }
