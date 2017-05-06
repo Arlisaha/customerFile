67d6cbedce7c70094a2e258a81768643189ba2eb
Yann Di Serio <diserio.yann@gmail.com>
2017-05-06 17:35:59 +0200

    Change price type in order to have money.

--- src/AppBundle/Form/CustomerCard/ConsultationType.php
+++ src/AppBundle/Form/CustomerCard/ConsultationType.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -62,7 +63,7 @@ class ConsultationType extends AbstractType
 			])
 			->add('location', TextType::class, [])
 			->add('duration', NumberType::class, [])
-			->add('price', NumberType::class, []);
+			->add('price', MoneyType::class, []);
 	}
 
 	public function configureOptions(OptionsResolver $resolver)
