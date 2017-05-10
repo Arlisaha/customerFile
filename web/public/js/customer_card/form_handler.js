98f192fdf25e737c2dd80128debc01fae4049c3a
Yann Di Serio <diserio.yann@gmail.com>
2017-05-10 23:19:44 +0200

    More styling and rendering.

--- web/public/js/customer_card/form_handler.js
+++ web/public/js/customer_card/form_handler.js
@@ -1,8 +1,8 @@
 /*var ownersData = {
-    id: '#customer_card_customer_owners',
-    prototype: '__owner__',
-    count: '{{ form.customer.owners|length }}'
-};*/
+ id: '#customer_card_customer_owners',
+ prototype: '__owner__',
+ count: '{{ form.customer.owners|length }}'
+ };*/
 
 function addTypePart(e, data) {
     e.preventDefault();
@@ -11,21 +11,38 @@ function addTypePart(e, data) {
     var list = $(data['append_id']);
 
     var newWidget = typePart.attr('data-prototype');
-    newWidget = newWidget.replace(new RegExp(data['prototype'], 'g'), data['count']);
-    data['count']++;
+    newWidget = newWidget.replace(new RegExp('__' + data['prototype'] + '__', 'g'), data['count']);
 
-    var newLi = $('<li></li>').html(newWidget);
+    if (data['count'] > 0) { //TODO : add hr only if not the first element of the list (animal)
+        var newHr = $('<hr />');
+        newHr.appendTo(list);
+    }
+
+    var newLi = $('<li id="' + data['prototype'] + '_' + data['count'] + '" class="visible"></li>').html(newWidget);
+
+    //TODO : collapse others li from the list
 
     newLi.appendTo(list);
+
     $('.froala').froalaEditor(froalaOptions);
     $('select').chosen(chosenOptions);
+
+    data['count']++;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.froala').froalaEditor(froalaOptions);
     $('select').chosen(chosenOptions);
-    $('#add_owner').click(function(e) {addTypePart(e, ownersData)});
-    $('#add_cat').click(function(e) {addTypePart(e, catsData)});
-    $('#add_dog').click(function(e) {addTypePart(e, dogsData)});
-    $('#add_consultation').click(function(e) {addTypePart(e, consultationsData)});
+    $('#add_owner').click(function (e) {
+        addTypePart(e, ownersData)
+    });
+    $('#add_cat').click(function (e) {
+        addTypePart(e, catsData)
+    });
+    $('#add_dog').click(function (e) {
+        addTypePart(e, dogsData)
+    });
+    $('#add_consultation').click(function (e) {
+        addTypePart(e, consultationsData)
+    });
 });
