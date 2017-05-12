19f9035c6dd8218c106a2dc0b2c67b16c5f6cac8
Yann Di Serio <diserio.yann@gmail.com>
2017-05-12 22:46:27 +0200

    Add more logic for basic others actions + basic views.

--- src/AppBundle/Controller/CustomerCardController.php
+++ src/AppBundle/Controller/CustomerCardController.php
@@ -2,11 +2,18 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Animal\AbstractAnimal;
+use AppBundle\Entity\Customer\Owner;
+use AppBundle\Entity\CustomerCard\Consultation;
 use AppBundle\Entity\CustomerCard\CustomerCard;
+use AppBundle\Form\Animal\SpecieType;
+use AppBundle\Form\CustomerCard\ConsultationType;
 use AppBundle\Form\CustomerCard\CustomerCardType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -18,41 +25,40 @@ use Symfony\Component\HttpFoundation\Request;
 class CustomerCardController extends Controller
 {
 	/**
-	 * @param $name
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 *
-	 * @Route("/", name="customer_card")
-	 */
-	public function indexAction()
-	{
-		$form = $this->createForm(CustomerCardType::class, null, []);
-
-		return $this->render(':customer_card:edit.html.twig', [
-			'form' => $form->createView(),
-		]);
-	}
-
-	/**
-	 * @param Request $request
-	 *
 	 * @Route("/all", name="customer_card_all")
 	 */
-	public function allAction(Request $request)
+	public function allAction()
 	{
-
+		$all = $this
+			->get('doctrine.orm.default_entity_manager')
+			->getRepository('AppBundle:CustomerCard\CustomerCard')
+			->findAll();
+		
+		$this->render(':customer_card:all.html.twig', [
+			'customer_cards' => $all,
+		]);
 	}
-
+	
 	/**
 	 * @param Request $request
 	 *
-	 * @Route("/list", name="customer_card_list")
+	 * @Route("/search", requirements={"owner_name"}, name="customer_card_search")
 	 */
-	public function listAction(Request $request)
+	public function searchAction(Request $request)
 	{
-
+		$form = $this->createFormBuilder(null)
+			->add('owner_lastname', TextType::class, [])
+			->add('owner_firstname', TextType::class, [])
+			->add('animal_name', TextType::class, [])
+			->add('specie', SpecieType::class, []);
+		
+		//TODO : handle form and search depending on criterias.
+		
+		$this->render(':customer_card:search.html.twig', [
+			'form' => $form,
+		]);
 	}
-
+	
 	/**
 	 * @param Request      $request
 	 * @param CustomerCard $customerCard
@@ -64,8 +70,28 @@ class CustomerCardController extends Controller
 	 */
 	public function getAction(Request $request, CustomerCard $customerCard)
 	{
+		$consultation = new Consultation();
+		$form         = $this->createForm(ConsultationType::class, $consultation, [
+			'customer_card_id' => $customerCard->getId(),
+		]);
+		
+		$form->handleRequest($request);
+		
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em = $this->get('doctrine.orm.default_entity_manager');
+			
+			$consultation->setCustomerCard($customerCard);
+			$em->persist($consultation);
+			
+			$em->flush();
+		}
+		
+		$this->render(':customer_card:get.html.twig', [
+			'customer_card' => $customerCard,
+			'form'          => $form,
+		]);
 	}
-
+	
 	/**
 	 * @param Request      $request
 	 * @param CustomerCard $customerCard
@@ -77,19 +103,45 @@ class CustomerCardController extends Controller
 	 */
 	public function editAction(Request $request, CustomerCard $customerCard)
 	{
-		$form = $this->createForm(CustomerCardType::class, $customerCard, []);
-
+		$form = $this->createForm(CustomerCardType::class, $customerCard, [
+			'action' => $this->generateUrl('customer_card_edit', ['id' => $customerCard->getId()]),
+			'method' => 'POST',
+		]);
+		
 		$form->handleRequest($request);
-
+		
 		if ($form->isSubmitted() && $form->isValid()) {
 			$em = $this->get('doctrine.orm.default_entity_manager');
 			
 			$em->persist($customerCard);
+			$customer = $customerCard->getCustomer();
+			
+			/** @var Consultation[] $consultations */
+			$consultations = $customerCard->getConsultations();
+			foreach ($consultations as $consultation) {
+				$consultation->setCustomerCard($customerCard);
+				$em->persist($consultation);
+			}
+			
+			/** @var Owner[] $owners */
+			$owners = $customer->getOwners();
+			foreach ($owners as $owner) {
+				$owner->setCustomer($customer);
+				$em->persist($owner);
+			}
+			
+			/** @var AbstractAnimal[] $animals */
+			$animals = $customer->getAnimals();
+			foreach ($animals as $animal) {
+				$animal->setCustomer($customer);
+				$em->persist($animal);
+			}
+			
 			$em->flush();
 			
 			return $this->redirectToRoute('customer_card_get', ['id' => $customerCard->getId()]);
 		}
-
+		
 		return $this->render(':customer_card:edit.html.twig', [
 			'form' => $form->createView(),
 		]);
