4885db136d421d642baa1d5d485771f5852ea1c5
Yann Di Serio <diserio.yann@gmail.com>
2017-06-05 10:14:17 +0200

    Made paginator method for customer card.

--- src/AppBundle/Repository/CustomerCard/CustomerCardRepository.php
+++ src/AppBundle/Repository/CustomerCard/CustomerCardRepository.php
@@ -3,8 +3,19 @@
 namespace AppBundle\Repository\CustomerCard;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class CustomerCardRepository extends EntityRepository
 {
+	public function findAllLimit($downLimit, $upLimit)
+	{
+		$qb = $this->createQueryBuilder('cc');
 
+		$qb
+			->orderBy('cc.id', 'ASC')
+			->setFirstResult($downLimit)
+			->setMaxResults($upLimit);
+
+		return new Paginator($qb);
+	}
 }
